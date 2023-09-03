824628b02d8b6d8dfde8964cdc5e97309da7e2a1
bhalaji.v <bhalaji702@gmail.com>
2023-09-04 00:22:09 +0530

    Task: Blog_app

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -23,5 +23,5 @@ class Comment < ApplicationRecord
   belongs_to :blog
   belongs_to :user
 
-  validates :comment, presence: { message: "cannot be blank" }
+  validates :comment, presence: true
 end
